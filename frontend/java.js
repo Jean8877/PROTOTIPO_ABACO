8a973769a7b27aff5769d5540da21ce0c4e9b88c
Andresvce <camiloandresvergelpedraza@gmail.com>
2025-09-02 11:06:01 -0500

    Update java.js

--- frontend/java.js
+++ frontend/java.js
@@ -12,15 +12,18 @@ function visualizar(data) {
                     <!--- boton de editar --->
                     <button type='button' class='btn btn-info'
                         onclick="location..href='.html?variable=${item.id_tipo_usuario}'">
-                         
-
-
-
-
-
-            </tr>  
-        
-    })
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m18.988 2.012l3 3L19.701 7.3l-3-3zM8 16h3l7.287-7.287l-3-3L8 13z"/><path fill="currentColor" d="M19 19H8.158c-.026 0-.053.01-.079.01c-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .896-2 2v14c0 1.104.897 2 2 2h14a2 2 0 0 0 2-2v-8.668l-2 2z"/></svg>
+                    </button>
+                </td>
+                <td>
+                    <!--- boton de eliminar --->
+                    <button type='button' class='btn btn-warning'
+                        onclick="eliminar(${item.id_tipo_usuario})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"/></svg>
+                    </button>
+                </td>
+            </tr>`;
+ })
 }
 
 function consulta_general(){
@@ -32,3 +35,6 @@ function consulta_general(){
     .then(data => visualizar(data))
     .catch(error => console.error('Error:', error));
 }
+
+
+// 
